ddb297a1f66eeeb96bf2eea0cf5808ce80b2201b
manueladinizc <manuela.mdc@gmail.com>
2022-10-03 21:06:18 -0300

    configurar Devise para usuário regular

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user_regulars
   devise_for :user_adms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
